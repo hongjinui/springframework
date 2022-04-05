dae4ded721428b01cf380f5b3d15f60053c86528
hongjinui <houago3@gmail.com>
2022-04-05 23:48:46 +0900

    2022-04-05 Member 작업

---
+++ src/main/generated/com/test/springframework/domain/entity/QItem.java
@@ -0,0 +1,57 @@
+package com.test.springframework.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QItem is a Querydsl query type for Item
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QItem extends EntityPathBase<Item> {
+
+    private static final long serialVersionUID = 985684512L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QItem item = new QItem("item");
+
+    public final QCategory category;
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final StringPath itmName = createString("itmName");
+
+    public final NumberPath<Integer> itmPrice = createNumber("itmPrice", Integer.class);
+
+    public final ListPath<OrderItem, QOrderItem> orderItems = this.<OrderItem, QOrderItem>createList("orderItems", OrderItem.class, QOrderItem.class, PathInits.DIRECT2);
+
+    public QItem(String variable) {
+        this(Item.class, forVariable(variable), INITS);
+    }
+
+    public QItem(Path<? extends Item> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QItem(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QItem(PathMetadata metadata, PathInits inits) {
+        this(Item.class, metadata, inits);
+    }
+
+    public QItem(Class<? extends Item> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new QCategory(forProperty("category")) : null;
+    }
+
+}
+
